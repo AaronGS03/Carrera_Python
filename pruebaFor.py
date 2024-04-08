673376d553e527c99f3a217446d82c7cc67c5734
Aaron <noesto34@gmail.com>
2024-04-08 12:45:17 +0200

    Dia 6 sesion 1 y cosas de la anterior sesion

---
+++ pruebaFor.py
@@ -0,0 +1,9 @@
+for var in range(1,11):
+    print(var,"")
+
+for var in range(10,0,-1):
+    print(var,"",end="")
+
+print("\n")
+for var in range(2,11,2):
+    print(var," ",end="")
---
+++ pruebaWhile.py
@@ -0,0 +1,7 @@
+cont="1234"
+intento= input("Contraseña: ")
+
+while intento!= cont:
+    print("Contraseña incorrecta")
+    intento= input("Contraseña: ")
+print("Acceso concedido")
