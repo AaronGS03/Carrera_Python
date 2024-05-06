82d1fa6e424e049cd7c803a4dfb6e4b219b60db4
AaronGS_03 <noesto34@gmail.com>
2024-05-06 17:40:27 +0200

    Reestructuración y comienzo curso ML

---
+++ MLwPython/introScikit.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import plotnine as pn
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+
+data_location= 'MLwPython\\tweets.csv'
+
+pd.options.display.max_columns= 500
+
+tweet_data= pd.read_csv(data_location)
+
+print(tweet_data.shape)
+
+print(tweet_data.head)
+
+
+X_variables = ['followers', 'video']
+y_variable = 'nlikes'
+model = LinearRegression()
+model.fit(tweet_data[X_variables], tweet_data[y_variable])
+tweet_data['predictions_lin_reg'] = model.predict(tweet_data[X_variables])
+model = DecisionTreeRegressor(random_state=0)
+model.fit(tweet_data[X_variables], tweet_data[y_variable])
+tweet_data['predictions_dt_reg'] = model.predict(tweet_data[X_variables])
+# Creamos un grafico simple para visualizar
+columns = [y_variable, 'predictions_lin_reg', 'predictions_dt_reg']
+graph_data = pd.melt(tweet_data[columns], y_variable)
+graph = (
+    pn.ggplot(graph_data, pn.aes(x='nlikes', y='value', color='variable')) 
+    + pn.geom_point(alpha=0.4)
+    + pn.scale_x_continuous(trans='log')
+    + pn.scale_y_continuous(trans='log')
+    + pn.ylab('prediction')
+
+)
+
+graph.draw()
